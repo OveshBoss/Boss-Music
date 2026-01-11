d66dc519e24f3ed59b032a50056beb8d9d4f2c4c
ovesh boss <oveshboss.08@gmail.com>
2026-01-11 13:01:52 +0530

    Update channeladmin.py

--- handlers/channeladmin.py
+++ handlers/channeladmin.py
@@ -9,9 +9,9 @@ from helpers.decorators import authorized_users_only, errors
 from helpers.filters import command, other_filters
 from callsmusic import callsmusic
 
+# --- Fixed all decorators below by removing ~filters.edited ---
 
-
-@Client.on_message(filters.command(["channelpause","cpause"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["channelpause","cpause"]) & filters.group)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
@@ -32,7 +32,7 @@ async def pause(_, message: Message):
         await message.reply_text("▶️ Music paused!")
 
 
-@Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["channelresume","cresume"]) & filters.group)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
@@ -53,7 +53,7 @@ async def resume(_, message: Message):
         await message.reply_text("⏸ Music resumed!")
 
 
-@Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["channelend","cend"]) & filters.group)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
@@ -77,7 +77,7 @@ async def stop(_, message: Message):
         await message.reply_text("⏹ Streaming ended!")
 
 
-@Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
+@Client.on_message(filters.command(["channelskip","cskip"]) & filters.group)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
