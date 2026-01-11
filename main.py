15f57a0da98e7bf91151f9127eb3493a2e6b600f
ovesh boss <oveshboss.08@gmail.com>
2026-01-11 13:33:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
-from pyrogram import Client as Bot
-
+from pyrogram import Client as Bot, filters # filters yahan add kiya hai
 from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
+
+# --- UNIVERSAL FIX FOR 'edited' ATTRIBUTE ERROR ---
+if not hasattr(filters, "edited"):
+    filters.edited = filters.create(lambda _, __, ___: False)
+# --------------------------------------------------
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
     file.write(response.content)
-
 
 bot = Bot(
     ":memory:",
